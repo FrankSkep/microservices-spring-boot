4e7c373ebd381619aee65185573e9c638ab97992
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-01-01 00:09:28 -0800

    microservice students

--- students-service/src/main/java/com/students/controller/StudentController.java
+++ students-service/src/main/java/com/students/controller/StudentController.java
@@ -1,10 +1,24 @@
 package com.students.controller;
 
+import com.students.entity.Student;
+import com.students.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id) {
+        return ResponseEntity.ok(studentService.getStudent(id));
+    }
 
 }
---
+++ students-service/src/main/java/com/students/service/StudentService.java
@@ -0,0 +1,7 @@
+package com.students.service;
+
+import com.students.entity.Student;
+
+public interface StudentService {
+    Student getStudent(Long id);
+}
