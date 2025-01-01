236f2ddcaed9c2e189da16265676fe0921922551
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-12-31 22:16:38 -0800

    add entity, controller, repository

---
+++ students-service/src/main/java/com/students/entity/Student.java
@@ -0,0 +1,26 @@
+package com.students.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    private Long id;
+
+    private String name;
+    private String lastname;
+    private String grade;
+
+}
