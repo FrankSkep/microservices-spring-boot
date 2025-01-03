88f948173f540c84c336b7c82cca3745703dffb0
Frank <f.cornejo.pacheco@gmail.com>
2025-01-03 10:51:23 -0800

    Delete students-service directory

--- students-service/src/main/java/com/students/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.students.repository;
-
-import com.students.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
--- students-service/src/main/java/com/students/service/StudentService.java
+++
@@ -1,7 +0,0 @@
-package com.students.service;
-
-import com.students.entity.Student;
-
-public interface StudentService {
-    Student getStudent(Long id);
-}
